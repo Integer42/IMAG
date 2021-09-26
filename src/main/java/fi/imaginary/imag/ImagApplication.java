18dc82635b615612d1128ea0c84622117f674741
Pyry <48580796+Integer42@users.noreply.github.com>
2021-09-26 16:32:07 +0300

    initial commit

---
+++ src/main/java/fi/imaginary/imag/ImagApplication.java
@@ -0,0 +1,13 @@
+package fi.imaginary.imag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/imaginary/imag/ImagApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.imaginary.imag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
